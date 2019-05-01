8f5c23ace09a61cf7b4fc4100d274af38c97c2c8
Mattible <mcallinan@MacBook-Pro.local>
2019-05-02 02:51:28 +1000

    updated env varibles in app, and updated version in package

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-
+// Outline a json response with the env variables
 function buildResponse() {
     return { 
         "myapplication": [
         {
-        "version": process.env.version,
+        "version": process.env.VERSION || "VERSION_not_found",
         "description" : "pre-interview technical test",
-        "lastcommitsha": process.env.lastcommitsha
+        "lastcommitsha": process.env.LAST_COMMIT || "LAST_COMMIT_not_found"
         }
        ]
     }
